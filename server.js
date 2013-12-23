c918699a14736c647b63543a957829967526422f
darulebreaker <chengj3.rpi@gmail.com>
2013-12-23 17:14:27 -0500

    very basic socket.io connection

---
+++ server.js
@@ -0,0 +1,34 @@
+/**
+ * Created by darulebreaker on 12/23/13.
+ */
+var app = require('http').createServer(handler)
+    , io = require('socket.io').listen(app)
+    , fs = require('fs')
+
+app.listen(8000);
+
+function handler (req, res) {
+    fs.readFile(__dirname + '/index.html',
+        function (err, data) {
+            if (err) {
+                res.writeHead(500);
+                return res.end('Error loading index.html');
+            }
+
+            res.writeHead(200);
+            res.end(data);
+        });
+}
+
+io.sockets.on('connect', function(socket){
+
+
+});
+
+
+io.sockets.on('connection', function (socket) {
+    socket.emit('news', { hello: 'world' });
+    socket.on('my other event', function (data) {
+        console.log(data);
+    });
+});

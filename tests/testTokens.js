cb284d68db7931d9206dd7ac6b69e34355c24d64
darulebreaker <chengj3.rpi@gmail.com>
2013-12-25 16:12:04 -0500

    added basic tokens

---
+++ tests/testTokens.js
@@ -0,0 +1,10 @@
+/**
+ * Created by darulebreaker on 12/25/13.
+ */
+var Tokens=require("./../game/Tokens");
+
+myNeutralToken = new Tokens.NeutralForce("starfall", new Array(4,5),"placeholder",3);
+myNeutralToken2 = new Tokens.NeutralForce("starfall",3,"placeholder",100);
+
+console.log(myNeutralToken);
+console.log(myNeutralToken2);

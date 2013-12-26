55db369989ffca6cbff8ac43d8cf4e794c1da808
eric <eric.ngeo@gmail.com>
2013-12-26 13:50:15 -0500

    eric commit two.... refactored the decks.

--- game/Decks.js
+++ game/Decks.js
@@ -2,58 +2,68 @@
  * Created by darulebreaker on 12/24/13.
  */
 var Cards = require("./../game/Cards");
-var Effects = require("./../game/Effects")
 
 // DECK IMPLEMENTATION
 
-function Deck() {
+var Deck = function() {
 	this.myCards = new Array();
-}
+};
 Deck.prototype.top = function() {
 	return this.myCards[0];
-}
+};
 Deck.prototype.popAndPushToBottom = function() {
 	top = this.myCards.shift();
 	this.myCards.push(top);
 	return top;
-}
+};
 Deck.prototype.shuffle = function() {
-	var i = deck.length, j, tempi, tempi;
+	var i = this.myCards.length, j, tempi, tempi;
 	if (i === 0)
 		return false;
 	while (--i) {
 		j = Math.floor(Math.random() * (i + 1));
-		tempi = deck[i];
-		tempj = deck[j];
-		deck[i] = tempj;
-		deck[j] = tempi;
+		tempi = this.myCards[i];
+		tempj = this.myCards[j];
+		this.myCards[i] = tempj;
+		this.myCards[j] = tempi;
 	}
-	return deck;
-}
+    return true;
+};
+Deck.prototype.push = function(card) {
+        this.myCards.push(card);
+};
 
 // END DECK
 
-function WesterosDeckOne() {
+function createWesterosDeckOne() {
 	// add a list of cards
 }
 
-function WesterosDeckTwo() {
+function createWesterosDeckTwo() {
 	// add a list of cards
 }
 
-function WesterosDeckThree() {
+function createWesterosDeckThree() {
+    myCards = new Deck();
+    myCards.push(new Cards.WesterosCards(1,"bullshit","bullshit","bullshit.png",0));
+    myCards.push(new Cards.WesterosCards(2,"bullshit2","bullshit2","bullshit2.png",0));
+    return myCards;
+}
+
+function createWildlingsDeck() {
 	// add a list of cards
 }
 
-function WildlingsDeck() {
+function createPostBattleDeck() {
 	// add a list of cards
 }
 
-function PostBattleCards() {
-	// add a list of cards
+function createHouseBattleDeck(house) {
+    // add a list of cards
 }
-
-exports.createWesteroDeck = createWesteroDeck;
-exports.shuffleDeck = shuffleDeck;
-exports.draw = draw;
-exports.drawOne = drawOne;
+exports.createWesterosDeckOne = createWesterosDeckOne;
+exports.createWesterosDeckTwo = createWesterosDeckTwo;
+exports.createWesterosDeckThree = createWesterosDeckThree;
+exports.createWildlingsDeck = createWildlingsDeck;
+exports.createPostBattleDeck = createPostBattleDeck;
+exports.createHouseBattleDeck = createHouseBattleDeck;
